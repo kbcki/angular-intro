a260ab2cc4a481462357953f46da5a4c43515cac
Jakub Kubacki <jakub.kubacki@synergycodes.com>
2025-06-02 21:38:55 +0200

    Add service

--- apps/angular-intro/src/app/app.component.ts
+++ apps/angular-intro/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CounterComponent } from '../counter/counter.component';
+import { CounterWithServiceComponent } from '../counter-with-service/counter-with-service.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CounterComponent],
+  imports: [CounterComponent, CounterWithServiceComponent],
 })
 export class AppComponent {
   title = 'angular-intro';
